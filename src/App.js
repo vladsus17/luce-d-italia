3b9d7bf4a31a3c1d29c582ab1545bbb70a0ccb2a
vladsus <you@example.com>
2025-02-18 13:03:02 +0000

    separete App in Categories

--- src/App.js
+++ src/App.js
@@ -1,23 +1,31 @@
-import logo from './logo.svg';
-import './App.css';
+import React from 'react';
+import './scss/app.scss';
+import Header from './components/Header';
+import Categories from './components/Categories';
+import Sort from './components/Sort';
+import PizzaBlock from './components/PizzaBlock';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div class="wrapper">
+      <Header />
+      <div class="content">
+        <div class="container">
+          <div class="content__top">
+            <Categories />
+            <Sort />
+          </div>
+          <h2 class="content_title">Todas las pizzas</h2>
+          <div class="content_items">
+            <PizzaBlock title="Barbacoa" price="8" /> <PizzaBlock title="4 Quesos" price="7" />
+            <PizzaBlock title="Mexicana" price="8" />
+            <PizzaBlock title="4 Quesos" price="7" />
+            <PizzaBlock title="Pizza Vlad" price="10" />
+            <PizzaBlock title="Margarita" price="6" />
+            <PizzaBlock title="PulledPork" price="7" />
+          </div>
+        </div>
+      </div>
     </div>
   );
 }
